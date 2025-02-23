9d7dfc8d975670cc0f4e03865111488e00da335c
Michael Cooper <coopermj@cs.toronto.edu>
2025-02-23 13:25:38 -0800

    Allow unoptimized images

--- next.config.js
+++ next.config.js
@@ -3,15 +3,19 @@
  */
 const nextConfig = {
   output: 'export',
- 
+  
+  images: {
+    unoptimized: true, // Allow unoptimized images
+  },
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
- 
+
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
- 
+
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
